6aa4077619e7e1c17eec720e60a41d81d6acd6d6
Nambinina-H <rasoanaivonambininahasina@gmail.com>
2025-04-08 08:04:19 +0300

    refactor: Remove unused Bell icon from Navbar component

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Bell, Search } from 'lucide-react';
+import { Search } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
@@ -21,10 +21,7 @@ const Navbar: React.FC<NavbarProps> = ({ className }) => {
       </div>
       
       <div className="flex items-center space-x-4">
-        <Button variant="ghost" size="icon" className="relative">
-          <Bell size={20} />
-          <span className="absolute top-1 right-1 w-2 h-2 bg-red-500 rounded-full"></span>
-        </Button>
+        
       </div>
     </header>
   );
